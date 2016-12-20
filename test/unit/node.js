171ab6a2b3b216d53653a0dfeddbef03610c2206
Pieter Delbeke <pieter.delbeke@zentrick.com>
2016-12-20 17:43:19 +0100

    Remove unnecessary async keywords

--- test/unit/node.js
+++ test/unit/node.js
@@ -1,29 +1,29 @@
 import VASTError from '../../src/index'
 
 describe('VastError', () => {
-  it('throws error', async () => {
+  it('throws error', () => {
     expect(() => {
       throw new VASTError()
     }).to.throw(Error)
   })
 
-  it('throws VastError', async () => {
+  it('throws VastError', () => {
     expect(() => {
       throw new VASTError()
     }).to.throw(VASTError)
   })
 
-  it('respects error code', async () => {
+  it('respects error code', () => {
     const err = new VASTError(100)
     expect(err.code).to.equal(100)
   })
 
-  it('supports custom error code', async () => {
+  it('supports custom error code', () => {
     const err = new VASTError(999)
     expect(err.code).to.equal(999)
   })
 
-  it('reports correct message', async () => {
+  it('reports correct message', () => {
     const err = new VASTError(100)
     expect(err.message).to.equal('100 (XML parsing error.)')
   })
